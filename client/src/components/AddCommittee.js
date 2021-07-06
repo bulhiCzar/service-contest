5ba60bd91695ad1035b0f62676471b909ee082a6
Denis Krasyuk <70818351+bulhiCzar@users.noreply.github.com>
2021-07-07 00:59:55 +0300

    add falidation name

--- client/src/components/AddCommittee.js
+++ client/src/components/AddCommittee.js
@@ -2,13 +2,14 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import {TextField} from "@material-ui/core";
-import React, {createContext, memo, useState} from "react";
+import React, { memo, useState} from "react";
 import countries from "countries-list"
 import {Autocomplete} from '@material-ui/lab';
 import Button from "@material-ui/core/Button";
 import {toast} from "react-toastify";
 import axios from "axios";
 import PhotoEditor from "./PhotoEditor";
+import {validateFullName} from "../utils/normalizeName";
 
 
 
@@ -30,7 +31,9 @@ const AddCommittee = ({setCommittee}) => {
         setData({...data, ...obj})
     }
     const handlerName = (e) => {
-        setData({...data, name: e.target.value})
+        const fullName = validateFullName({fullName: e.target.value}).replaceToUpperCaseFirstSymbol()
+        console.log(fullName)
+        setData({...data, name: fullName})
     }
     const handlerTypeLink = (e) => {
         setData({...data, typeLink: e.target.value})
